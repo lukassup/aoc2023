341695103eecdf797256a20441d49b5024dfa27b
Lukas Šupienis <lukas.supienis@gmail.com>
2023-12-04 17:58:22 +0200

    day4 pt2

--- src/day4.rs
+++ src/day4.rs
@@ -13,42 +13,65 @@ where
 pub fn day4pt1(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
     let path = Path::new(filename);
     let lines = read_lines(path)?.flatten();
-    let results: Vec<u64> = lines
-        .filter_map(|line| {
-            let (part1, part2) = line.split_once(':')?;
-            let card_num = part1.split_once(' ')?.1.trim().parse::<u64>().ok()?;
-            let (win_str, draw_str) = part2.split_once('|')?;
-            let win_nums: Vec<u64> = win_str
-                .split(' ')
-                .filter_map(|n| n.trim().parse::<u64>().ok())
-                .collect();
-            let draw_nums: Vec<u64> = draw_str
-                .split(' ')
-                .filter_map(|n| n.trim().parse::<u64>().ok())
-                .collect();
-            let score = win_nums.iter().fold(0, |result, win_num| {
-                if draw_nums.contains(win_num) {
-                    if result == 0 {
-                        1
-                    } else {
-                        result * 2
-                    }
-                } else {
-                    result
-                }
-            });
-            Some(score)
-        })
-        .collect();
-    Ok(results.iter().sum())
+    let results_iter = lines.filter_map(|line| {
+        let part2 = line.split_once(':')?.1;
+        let (win_str, draw_str) = part2.split_once('|')?;
+        let win_nums_iter = win_str
+            .split(' ')
+            .filter_map(|n| n.trim().parse::<u64>().ok());
+        let draw_nums: Vec<u64> = draw_str
+            .split(' ')
+            .filter_map(|n| n.trim().parse::<u64>().ok())
+            .collect();
+        let win_count = win_nums_iter
+            .filter(|win_num| draw_nums.contains(&win_num))
+            .count() as u32;
+        if win_count > 0 {
+            Some(2u64.pow(win_count - 1))
+        } else {
+            None
+        }
+    });
+    Ok(results_iter.sum())
 }
 
 pub fn day4pt2(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
     let path = Path::new(filename);
     let lines = read_lines(path)?.flatten();
-    let results: Vec<u64> = Vec::new();
-    // TODO
-    Ok(results.iter().sum())
+    let card_scores: Vec<u64> = lines
+        .filter_map(|line| {
+            let part2 = line.split_once(':')?.1;
+            let (win_str, draw_str) = part2.split_once('|')?;
+            let win_nums_iter = win_str
+                .split(' ')
+                .filter_map(|n| n.trim().parse::<u64>().ok());
+            let draw_nums: Vec<u64> = draw_str
+                .split(' ')
+                .filter_map(|n| n.trim().parse::<u64>().ok())
+                .collect();
+            let win_count = win_nums_iter
+                .filter(|win_num| draw_nums.contains(&win_num))
+                .count() as u64;
+            Some(win_count)
+        })
+        .collect();
+
+    // build vector to store count of card copies, we start with one copy for each card
+    let mut card_copies: Vec<(&u64, u64)> = card_scores
+        .iter()
+        .map(|card_score| (card_score, 1))
+        .collect();
+    // count per-card copies
+    for i in 0..card_copies.len() {
+        let (score, copies_initial) = card_copies[i];
+        if *score > 0 {
+            for j in (i + 1)..(i + 1 + *score as usize) {
+                card_copies[j].1 += copies_initial;
+            }
+        }
+    }
+    let result = card_copies.iter().fold(0, |sum, (_, copies)| sum + copies);
+    Ok(result)
 }
 
 #[allow(unused_imports)]
