f0e1042e45e416d8f417a9abfd3707afb5d60cb7
Lukas Šupienis <lukas.supienis@gmail.com>
2023-12-04 12:27:43 +0200

    day4pt1

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,3 @@
-use regex::{Match, Regex};
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -13,16 +12,41 @@ where
 
 pub fn day4pt1(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
     let path = Path::new(filename);
-    let mut lines = read_lines(path)?.flatten();
-    let mut results: Vec<u64> = Vec::new();
-    // TODO
+    let lines = read_lines(path)?.flatten();
+    let results: Vec<u64> = lines
+        .filter_map(|line| {
+            let (part1, part2) = line.split_once(':')?;
+            let card_num = part1.split_once(' ')?.1.trim().parse::<u64>().ok()?;
+            let (win_str, draw_str) = part2.split_once('|')?;
+            let win_nums: Vec<u64> = win_str
+                .split(' ')
+                .filter_map(|n| n.trim().parse::<u64>().ok())
+                .collect();
+            let draw_nums: Vec<u64> = draw_str
+                .split(' ')
+                .filter_map(|n| n.trim().parse::<u64>().ok())
+                .collect();
+            let score = win_nums.iter().fold(0, |result, win_num| {
+                if draw_nums.contains(win_num) {
+                    if result == 0 {
+                        1
+                    } else {
+                        result * 2
+                    }
+                } else {
+                    result
+                }
+            });
+            Some(score)
+        })
+        .collect();
     Ok(results.iter().sum())
 }
 
 pub fn day4pt2(filename: &str) -> Result<u64, Box<dyn std::error::Error>> {
     let path = Path::new(filename);
-    let mut lines = read_lines(path)?.flatten();
-    let mut results: Vec<u64> = Vec::new();
+    let lines = read_lines(path)?.flatten();
+    let results: Vec<u64> = Vec::new();
     // TODO
     Ok(results.iter().sum())
 }
